0197f7fac155d6d5ce70248f346a178e84c6a278
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-24 09:28:24 +0530

    Question - Unknown route

--- index.js
+++ index.js
@@ -2,34 +2,27 @@ const express = require("express")
 const app = express()
 
 
-app.use("/library", (req, res, next) => {
-  res.send("Entry to library");
-  next()
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.")
+})
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.")
+})
+
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.")
+})
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.")
+})
+
+app.use("*", (req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>")
 })
 
 
-const libraryEntry = (req, res, next) => {
-  req.user = "Guest"
-  next()
-} 
-
-app.get("/orders", (req, res) => {
-  res.send("Here is the list of all orders")
-})
-
-app.post("/orders", (req, res) => {
-  res.send("A new order has been created.")
-})
-
-app.get("/users", (req, res) => {
-  res.send("Here is the list of all users.")
-})
-
-app.post("/users", (req, res) => {
-  res.send("A new user has been added.")
-})
-
-
-app.listen(3000, () => {
-  console.log("Server running on PORT 3000");
+app.listen(4000, () => {
+  console.log("Server running on PORT 4000");
 })
