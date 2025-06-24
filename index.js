d9c2db8a03fc941797d64fe987696e0e3aff49de
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-24 10:15:50 +0530

    What are Dynamic Routes and Why do we need it ?

--- index.js
+++ index.js
@@ -2,27 +2,15 @@ const express = require("express")
 const app = express()
 
 
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.")
-})
+app.get("/welcome/:username", (req, res) => {
+  const username = req.params.username
+  const role = req.query.role
 
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.")
+  res.send(`Hello ${username}, your role is ${role}`)
 })
-
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.")
-})
-
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.")
-})
-
-app.use((req, res) => {
-  res.status(404).send("<h1>404 - Page Not Found</h1>")
-})
-
 
 app.listen(4000, () => {
   console.log("Server running on PORT 4000");
 })
+
+
