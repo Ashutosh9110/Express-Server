6315962e5bcab47a488311ea8117c751ada8f225
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-24 09:02:54 +0530

    Create Your Own Middleware and Routing

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+
+app.use("/library", (req, res, next) => {
+  res.send("Entry to library");
+  next()
 })
 
 
+const libraryEntry = (req, res, next) => {
+  req.user = "Guest"
+  next()
+} 
+
+app.get("/welcome", libraryEntry, (req, res) => {
+  res.send(`<h1>Welcome ${req.user}</h1>`);
+})
+  
+
 app.listen(3000, () => {
-  console.log("Server is running on PORT 3000");
+  console.log("Server running on PORT 3000");
 })
