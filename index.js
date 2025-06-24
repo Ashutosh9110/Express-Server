cb525a7c04cd1cdfe39422470b5279d1eb1c5038
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-24 12:09:13 +0530

    Router and Filters

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
   const express = require("express")
   const app = express()
+  const orderRouter = require("./routes/orders")
+  const userRouter = require("./routes/users")
+  // app.use((req, res, next) => {
+  //   console.log(`${req.method} sent request to ${req.url}`);
+  //   next()
+  // })
+
+  app.use("/products", orderRouter)
+  app.use("/users", userRouter)
 
 
-  app.use((req, res, next) => {
-    console.log(`${req.method} sent request to ${req.url}`);
-    next()
-  })
-
-  app.get("/products", (req, res) => {
-    res.send("Here is the list of all products");
-  })
-
-  app.post("/products", (req, res) => {
-    res.send("A new product has been added.")
-  })
-
-  app.get("/categories", (req, res) => {
-    res.send("Here is the list of all categories.")
-  })
-
-  app.post("/categories", (req, res) => {
-    res.send("A new category has been created.")
-  })
 
 
   app.listen(4000, () => {
